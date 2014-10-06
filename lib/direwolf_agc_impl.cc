e0bf4e4c90208a735c6601f16f7c90426ae22044
Tim Kuester <tkuester@users.noreply.github.com>
2014-10-06 19:36:58 -0400

    Debug outputs. Now removing DC component

--- lib/direwolf_agc_impl.cc
+++ lib/direwolf_agc_impl.cc
@@ -41,7 +41,7 @@ namespace gr {
     direwolf_agc_impl::direwolf_agc_impl(float attack, float decay)
       : gr::sync_block("direwolf_agc",
               gr::io_signature::make(1, 1, sizeof(float)),
-              gr::io_signature::make(1, 1, sizeof(float))),
+              gr::io_signature::make(1, 3, sizeof(float))),
         d_attack(attack), d_decay(decay),
         d_peak(1), d_valley(0)
     {
@@ -61,6 +61,14 @@ namespace gr {
     {
         const float *in = (const float*) input_items[0];
         float *out = (float*) output_items[0];
+        float *peak_out = NULL;
+        float *valley_out = NULL;
+
+        if(output_items.size() == 3)
+        {
+            peak_out = (float*)output_items[1];
+            valley_out = (float*)output_items[2];
+        }
 
         // Taken from WB2OSZ's Direwolf demodulator
         for(int i = 0; i < noutput_items; i++)
@@ -73,6 +81,10 @@ namespace gr {
             {
                 d_peak = in[i] * d_decay + d_peak * (1.0 - d_decay);
             }
+            if(peak_out != NULL)
+            {
+                peak_out[i] = d_peak;
+            }
 
             if(in[i] < d_valley)
             {
@@ -82,10 +94,22 @@ namespace gr {
             {
                 d_valley = in[i] * d_decay + d_valley * (1.0 - d_decay);
             }
+            if(valley_out != NULL)
+            {  
+                valley_out[i] = d_valley;
+            }
 
             if(d_peak > d_valley)
             {
-                out[i] = in[i] / (d_peak - d_valley);
+                out[i] = (in[i] - 0.5 * (d_peak + d_valley)) / (d_peak - d_valley);
+                if(out[i] > 5)
+                {
+                    out[i] = 5;
+                }
+                else if(out[i] < -5)
+                {
+                    out[i] = -5;
+                }
             }
             else
             {
